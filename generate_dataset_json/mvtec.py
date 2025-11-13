550fa6a942455deeb414014abc3636fafa30226f
秦勇（qinyong） <60870424+qinyong123456@users.noreply.github.com>
2025-11-13 15:49:34 +0800

    Refactor MVTecSolver class and update root path

--- generate_dataset_json/mvtec.py
+++ generate_dataset_json/mvtec.py
@@ -9,7 +9,7 @@ class MVTecSolver(object):
         'tile', 'toothbrush', 'transistor', 'wood', 'zipper',
     ]
 
-    def __init__(self, root='/kaggle/input/mvtec-ad'):
+    def __init__(self, root='data/mvtec'):
         self.root = root
         self.meta_path = f'{root}/meta.json'
 
@@ -47,6 +47,7 @@ class MVTecSolver(object):
             f.write(json.dumps(info, indent=4) + "\n")
         print('normal_samples', normal_samples, 'anomaly_samples', anomaly_samples)
 if __name__ == '__main__':
-    runner = MVTecSolver(root='/remote-home/iot_zhouqihang/data/mvdataset')
+    runner = MVTecSolver(root='/kaggle/input/mvtec-ad')
     runner.run()
+
 
